7f26cedd811ca1dc8895288c467ca0e02d299a1d
Hongli Lai <honglilai@gmail.com>
2019-06-24 14:03:42 +0200

    ...

--- vars/cookbookPipeline.groovy
+++ vars/cookbookPipeline.groovy
@@ -28,6 +28,15 @@ def call(options) {
                         echo "Current commit: ${env.GIT_COMMIT}"
                         echo "Previous successful commit: ${env.GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
 
+                        checkout scm
+                        // clean the workspace before we begin, if we don't do this it keep files
+                        // like Berksfile.lock and that might give conflicts when running `berks install`
+                        sh 'git clean -dXf'
+                        if (fileExists('Gemfile')) {
+                            sh 'bundle install -j 4 --path=$HOME/bundle'
+                        }
+                        //sh 'berks install'
+
                         def metadataFile = readFile(getMetadataFileFullPath())
                         def versionString = extractVersionString(metadataFile)
                         def versionArray = HELPER.parseVersionString(METADATA_FILE_BASE_NAME, versionString)
@@ -49,6 +58,51 @@ def call(options) {
                             options['prepare']()
                         }
                     }
+                }
+            }
+            stage('Tests') {
+                script {
+                    def usesBundler = fileExists('Gemfile')
+                    def shRubyCommand = { command ->
+                        if (usesBundler) {
+                            sh "bundle exec ${command}"
+                        } else {
+                            sh command
+                        }
+                    }
+
+                    def tests = {
+                        foodcritic: {
+                            shRubyCommand 'foodcritic .'
+                        }
+                    }
+
+                    if (fileExists('spec')) {
+                        tests['rspec'] = {
+                            if (usesBundler) {
+                                sh 'bundle exec rspec -f d'
+                            } else {
+                                sh 'chef exec rspec -f d'
+                            }
+                        }
+                    }
+
+                    if (fileExists('.kitchen.yml') || fileExists('kitchen.yml')) {
+                        tests['kitchen'] = {
+                            try {
+                                shRubyCommand 'kitchen converge'
+                                shRubyCommand 'kitchen verify'
+                            } finally {
+                                shRubyCommand 'kitchen destroy'
+                            }
+                        }
+                    }
+
+                    if (options.containsKey('tests')) {
+                        tests += options['tests']
+                    }
+
+                    parallel(tests)
                 }
             }
         }
